d76039c0906466f3bd8dd35a825741f72294490d
jjaworska-hub <77283583+jjaworska-hub@users.noreply.github.com>
2024-10-20 21:04:08 +0200

    Add files via upload

---
+++ Zad 4 - 3 na 3/Zad 4 - 3 na 3/Zad 4 - 3 na 3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <algorithm> 
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Podaj trzy liczby: ";
+    cin >> a >> b >> c;
+    
+    int max1 = max(a, b); 
+    int max2 = max(max1, c);
+
+    cout << "Największa liczba to: " << max2 << endl;
+
+    return 0;
+}
